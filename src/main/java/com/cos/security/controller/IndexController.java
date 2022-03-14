f99d2a019182f62b826072d13a589d731ca1ec76
hyena0608 <goatsfish@gmail.com>
2022-03-14 22:38:44 +0900

    init

---
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -0,0 +1,45 @@
+package com.cos.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping({"", "/"})
+    public String index() {
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/manager")
+    public String manager() {
+        return "manager";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/join")
+    public String join() {
+        return "join";
+    }
+
+    @GetMapping("/joinProc")
+    public @ResponseBody
+    String joinProc() {
+        return "회원가입 완료됨!";
+    }
+}
