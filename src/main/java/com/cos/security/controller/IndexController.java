577e55c49547c5e934defa15cd8f04909de1da1a
hyena0608 <goatsfish@gmail.com>
2022-03-14 23:17:08 +0900

    SecurityConfig 작성

--- src/main/java/com/cos/security/controller/IndexController.java
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.cos.security.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class IndexController {
@@ -13,27 +14,27 @@ public class IndexController {
     }
 
     @GetMapping("/user")
-    public String user() {
+    public @ResponseBody String user() {
         return "user";
     }
 
     @GetMapping("/admin")
-    public String admin() {
+    public @ResponseBody String admin() {
         return "admin";
     }
 
     @GetMapping("/manager")
-    public String manager() {
+    public @ResponseBody String manager() {
         return "manager";
     }
 
     @GetMapping("/login")
-    public String login() {
+    public @ResponseBody String login() {
         return "login";
     }
 
     @GetMapping("/join")
-    public String join() {
+    public @ResponseBody String join() {
         return "join";
     }
 
