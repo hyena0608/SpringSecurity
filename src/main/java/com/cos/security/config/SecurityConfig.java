717f0e40c67fcc973024f48aa98b667308a102ec
hyena0608 <goatsfish@gmail.com>
2022-03-17 23:53:07 +0900

    feat(login): 시큐리티 대신 로그인하게 SecurityConfig 설정

--- src/main/java/com/cos/security/config/SecurityConfig.java
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -27,7 +27,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().permitAll()
                 .and()
                 .formLogin()
-                .loginPage("/loginForm");
-
+                .loginPage("/loginForm")
+                .loginProcessingUrl("/login") //login 주소가 호출 되면 시큐리티가 낚아채서 대신 로그인을 진행해준다.
+                .defaultSuccessUrl("/");
     }
 }
