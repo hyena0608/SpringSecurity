1d3a4ee3dd83212be614b9ba51e773b14433ae27
hyena0608 <goatsfish@gmail.com>
2022-03-18 07:12:36 +0900

    feat(UserRepository): 유저네임으로 유저 검색 기능 구현

--- src/main/java/com/cos/security/repository/UserRepository.java
+++ src/main/java/com/cos/security/repository/UserRepository.java
@@ -6,4 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // CRUD 함수를 JpaRepository가 들고 있다.
 // @Repository 가 없어도 IOC -> JpaRepository 상속하고 있으니
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    /**
+     * findBy 규칙 -> Username 문법
+     * select * from user where username = 1?
+     */
+    public User findByUsername(String username); // JPA Query Method
 }
