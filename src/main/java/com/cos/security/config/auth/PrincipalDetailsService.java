a72d021afc7af5f5d6c20004ddecf213e8f286a2
hyena0608 <goatsfish@gmail.com>
2022-03-18 07:09:45 +0900

    feat(UserDetailsService): /login 호출 시 username 받아오게 설정

---
+++ src/main/java/com/cos/security/config/auth/PrincipalDetailsService.java
@@ -0,0 +1,26 @@
+package com.cos.security.config.auth;
+
+import com.cos.security.model.User;
+import com.cos.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+/**
+ * 시큐리티 설정에서 loginProcessingUrl("/login");
+ * /login 요청이 오면 자동으로 UserDetailService 타입으로 IoC되어 있는 loadUserByUsername 함수가 실행
+ */
+@Service
+public class PrincipalDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User userEntity = userRepository.findByUsername(username);
+        return null;
+    }
+}
