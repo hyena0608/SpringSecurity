0426cc16a36c7a5ed687a5a3159fca829060eaf8
hyena0608 <goatsfish@gmail.com>
2022-03-18 22:52:03 +0900

    feat(DefaultOAuth2UserService): 구글 회원 정보 받아오기

--- src/main/java/com/cos/security/config/SecurityConfig.java
+++ src/main/java/com/cos/security/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security.config;
 
+import com.cos.security.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -9,10 +11,21 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+/**
+ * 1. 코드반기(인증)
+ * 2. 엑세스토큰(권한)
+ * 3. 사용자 프로필 정보를 가져오기
+ * 4-1. 그 정보를 토대로 회원가입 자동으로 진행
+ * 4-2. (이메일, 전화번호, 이름, 아이디) 쇼핑몰 -> (집주소), 백화점몰 -> (VIP등급, 일반등급)
+ */
+
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터 체인에 등록이 된다.
 @EnableGlobalMethodSecurity(securedEnabled = true, prePostEnabled = true) // secured 어노테이션 활성화, preAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
     @Bean
     public BCryptPasswordEncoder encodePwd() {
@@ -34,6 +47,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm"); // 구글 로그인이 완료된 뒤의 후처리가 필요
+                .loginPage("/loginForm")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService); // 구글 로그인이 완료된 뒤의 후처리 필요 tip. 코드X (엑세스토큰 + 사용자프로필 정보)
     }
 }
---
+++ src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
@@ -0,0 +1,22 @@
+package com.cos.security.config.oauth;
+
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
+
+    // 구글로 부터 받은 userRequest 데이터에 대한 후처리되는 함수
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        System.out.println("getClientRegistration: " + userRequest.getClientRegistration());
+        System.out.println("getAccessToken: " + userRequest.getAccessToken().getTokenValue());
+        System.out.println("getAttribute: " + super.loadUser(userRequest).getAttributes());
+
+        // 회원가입을 강제로 진행해볼 예정
+       return super.loadUser(userRequest);
+    }
+}
