17dec66ab5e0353651c63f93f78cc185a93b1c79
hyena0608 <goatsfish@gmail.com>
2022-03-20 08:52:09 +0900

    일반이 아닌 구글 OAuth 로그인일 시에 형 다운캐스팅

--- src/main/java/com/cos/security/controller/IndexController.java
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,6 +32,15 @@ public class IndexController {
 
         System.out.println("userDetails = " + userDetails.getUser());
         return "세션 정보 확인하기";
+    }
+
+    @GetMapping("/test/oauth/login")
+    public @ResponseBody String testOAuthLgin(Authentication authentication) {
+        System.out.println("/test/oauth/login ==============");
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        System.out.println("authentication = " + oAuth2User.getAttributes());
+
+        return "OAuth 세션 정보 확인하기";
     }
 
     @GetMapping({"", "/"})
