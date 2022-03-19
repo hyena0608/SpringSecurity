51e4adf78f93d72f69d5c8da3724a4bc9e3a100b
hyena0608 <goatsfish@gmail.com>
2022-03-20 08:58:39 +0900

    oauth 구글 로그인 @AuthenticationPrincipal 어노테이션으로 받고 OAuth2User로 다운 캐스팅하여 정보 받기

--- src/main/java/com/cos/security/controller/IndexController.java
+++ src/main/java/com/cos/security/controller/IndexController.java
@@ -35,11 +35,11 @@ public class IndexController {
     }
 
     @GetMapping("/test/oauth/login")
-    public @ResponseBody String testOAuthLgin(Authentication authentication) {
+    public @ResponseBody String testOAuthLgin(Authentication authentication, @AuthenticationPrincipal OAuth2User oauth) { // DI 의존성 주입
         System.out.println("/test/oauth/login ==============");
         OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
         System.out.println("authentication = " + oAuth2User.getAttributes());
-
+        System.out.println("oAuth2User = " + oauth.getAttributes());
         return "OAuth 세션 정보 확인하기";
     }
 
