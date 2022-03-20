982288a582a556a8f2a9a78e2553e3e52a32d4b3
hyena0608 <goatsfish@gmail.com>
2022-03-20 10:48:50 +0900

    fix: 구글 로그인 후 처리, 회원 프로필 정보 OAuth2User로 수정

--- src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/cos/security/config/oauth/PrincipalOauth2UserService.java
@@ -14,11 +14,13 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         System.out.println("getClientRegistration: " + userRequest.getClientRegistration()); // registrationId로 어떤 OAuth로 로그인 헀는지 확인 가능
         System.out.println("getAccessToken: " + userRequest.getAccessToken().getTokenValue());
-        // 구글 로그인 버튼 클릭 -> 구글 로그인 창 -> 로그인을 완료 -> code를 리턴(OAuth-Client 라이브러리) -> AccessToken 요청
-        // userRequest 정보-> loadUser함수 호출 -> 구글로부터 회원 프로필 받아준다.
-        System.out.println("getAttribute: " + super.loadUser(userRequest).getAttributes());
 
         OAuth2User oAuth2User = super.loadUser(userRequest);
+
+        // 구글 로그인 버튼 클릭 -> 구글 로그인 창 -> 로그인을 완료 -> code를 리턴(OAuth-Client 라이브러리) -> AccessToken 요청
+        // userRequest 정보-> loadUser함수 호출 -> 구글로부터 회원 프로필 받아준다.
+        System.out.println("getAttribute: " + oAuth2User.getAttributes());
+
         // 회원가입을 강제로 진행해볼 예정
        return super.loadUser(userRequest);
     }
